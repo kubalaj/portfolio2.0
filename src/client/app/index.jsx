67421ee285d31c3f72247387100cc2ba371d65ea
Paul Kubala <paulisdaman@gmail.com>
2017-02-20 09:58:43 -0700

    getting less loader in

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {render} from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx';
 
+require("!style-loader!css-loader!less-loader!./main.less");
+
+
 class App extends React.Component {
   render () {
     return (
