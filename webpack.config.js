4751399f3e6eebd1e462a7d426df240159a38dc8
Paul Kubala <paulisdaman@gmail.com>
2017-02-20 10:01:22 -0700

    getting stlyes in loaded

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ var config = {
         test : /\.jsx?/,
         include : APP_DIR,
         loader : 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: "less-loader"
       }
     ]
   }
